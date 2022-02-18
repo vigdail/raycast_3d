bd2c29313b1c49fef1d6db3c993e336daca04047
kmb2 <kmb2@tpu.ru>
2022-02-18 17:18:21 +0600

    Render top down map

--- main.cpp
+++ main.cpp
@@ -1,17 +1,71 @@
 #include <SDL.h>
 
+#include <array>
 #include <iostream>
 
-constexpr int SCREEN_WIDTH = 640;
-constexpr int SCREEN_HEIGHT = 480;
+constexpr int SCREEN_WIDTH = 1280;
+constexpr int SCREEN_HEIGHT = 512;
+
+constexpr size_t CELL_SIZE = 64;
+constexpr size_t MAP_WIDTH = 10;
+constexpr size_t MAP_HEIGHT = 8;
+
+struct Player {
+  SDL_Point pos{100, 100};
+  float angle{0.0f};
+};
+
+using Map = std::array<int, MAP_WIDTH * MAP_HEIGHT>;
+
+Player player;
+constexpr Map map = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1, //
+    1, 0, 0, 0, 0, 0, 1, 1, 0, 1, //
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1, //
+    1, 0, 0, 0, 1, 1, 1, 0, 0, 1, //
+    1, 0, 0, 0, 0, 0, 1, 0, 0, 1, //
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1, //
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //
+};
 
 void printSdlError(const char *title) {
   std::cerr << title << ": " << SDL_GetError() << '\n';
 }
 
+void renderMap(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xff, 0xff);
+  for (size_t i = 0; i < map.size(); ++i) {
+    if (map[i] == 0) {
+      continue;
+    }
+    int x = static_cast<int>(i % MAP_WIDTH * CELL_SIZE);
+    int y = static_cast<int>(i / MAP_WIDTH * CELL_SIZE);
+    SDL_Rect rect{x, y, CELL_SIZE, CELL_SIZE};
+    SDL_RenderFillRect(renderer, &rect);
+  }
+}
+
+void renderPlayer(SDL_Renderer *renderer) {
+  const int ray_len = 30;
+  SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+  int ray_x = player.pos.x + ray_len * static_cast<int>(cos(player.angle));
+  int ray_y = player.pos.y + ray_len * static_cast<int>(sin(player.angle));
+  SDL_RenderDrawLine(renderer, player.pos.x, player.pos.y, ray_x, ray_y);
+
+  const int player_size = 5;
+  SDL_SetRenderDrawColor(renderer, 0xff, 0x00, 0x00, 0xff);
+  SDL_Rect rect{player.pos.x - player_size / 2, player.pos.y - player_size / 2,
+                player_size, player_size};
+  SDL_RenderFillRect(renderer, &rect);
+}
+
 void render(SDL_Renderer *renderer) {
   SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
   SDL_RenderClear(renderer);
+
+  renderMap(renderer);
+  renderPlayer(renderer);
 
   SDL_RenderPresent(renderer);
 }
